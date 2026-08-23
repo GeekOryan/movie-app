18348277ddd7cadf9873540ecf42b896efb3c5f0
GeekOryan <225357530+GeekOryan@users.noreply.github.com>
2026-08-23 20:28:07 +0200

    Added svg icons and logo imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-
-const Card = ({ title }) => {
-
-  const [hasLiked, setHasLiked] = useState(false);
-
-  return (
-    <div className='card' style={{
-      border: '1px solid #4b5362',
-      padding: '20px',
-      margin: '10px',
-      backgroundColor: '#31363',
-      borderRadius: '10px',
-      minHeight: '100px',
-    }}>
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️": "🤍"}
-      </button>
-    </div>
-  )
-}
+import React from 'react';
 
 const App = () => {
-  return ( 
-    <div className="card-container">
-      <Card title="Avengers Doomsday" rating={10} actors={[{ name: "Robert Downey JR.", name: "Chris Hemsworth"}]}/>
-      <Card title="Dune 3" />
-    </div>
+  return (
+    <main>
+      
+      <div className="pattern" />
+
+      <div className="wrapper" />
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+    
+    </main>
   )
 }
 
